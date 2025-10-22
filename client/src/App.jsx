237dc32d06178b36d03bc919453d42cae9b5e0dc
farhanOkobiz <farhan.okobiz@gmail.com>
2025-10-22 13:01:26 +0600

    App page updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ import UserProfile from "./pages/UserProfile";
 
 import CheckOut from "./pages/Checkout";
 import OrderConfirmation from "./pages/OrderConfirmation";
-import Thankyou from "./pages/Thankyou";
+import Thankyou from "./pages/ThankYou";
 import ForgotPassword from "./pages/ForgotPassword";
 
 import VerifyEmail from "./pages/VerifyEmail";
